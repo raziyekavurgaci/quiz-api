6afa2aceab4461b4c81cef6761800fa8cffbf013
raziyekavurgaci <raziyekavurgaci1234@gmail.com>
2025-07-15 23:48:23 +0300

    Updated the 'endOfLine' value to 'lf' in the Prettier settings. Modified the module settings in the TypeScript configuration, added new compiler options, and removed unnecessary lines. Added a comma to the provider list in the AuthModule. Updated the exports in the SharedModule by adding new DTOs.

---
+++ src/shared/dto/login-dto.ts
@@ -0,0 +1,21 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class RegisterUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  @MaxLength(20)
+  password: string;
+}
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { LoginDto } from './dto/login-dto';
+import { RegisterDto } from './dto/register-dto';
 @Module({
-  exports: [CreateUserDto, UpdateUserDto],
+  exports: [CreateUserDto, UpdateUserDto, LoginDto, RegisterDto],
 })
 export class SharedModule {}
